8aaa30aae0a1bbf4c74f759cfce69a6f65f97d70
Belawal Umer <belawal.umer@pakeventures.com>
2021-01-19 17:21:11 +0500

    resolved webpacker issue

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   root 'products#index'
   resources :products
+
+  get "/admin", to: "users#admin"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
